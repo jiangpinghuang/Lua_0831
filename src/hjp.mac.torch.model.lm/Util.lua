9a153951abd135440f23c8599344b31f7ea382fb
hjpwhu <hjpwhu@qq.com>
2016-08-22 10:00:57 +0800

    ini

--- src/hjp.mac.torch.model.lm/Util.lua
+++
@@ -1,7 +0,0 @@
---[[
--- Copyright (c) 2016, by Jiang.ping.Huang.
--- All rights reserved.
-
--- Neural language model for normalization, based on long short-term memory.
--- LICENSE file will be added in the root directory of this source folder.
-]]--
